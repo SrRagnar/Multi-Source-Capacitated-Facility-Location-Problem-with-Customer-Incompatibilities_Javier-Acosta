56d577e9c9a49fdfd18685d69cb2e6aa0834ff3d
Javier Acosta Portocarrero <alu0101660769@ull.edu.es>
2026-04-05 08:37:39 +0000

    Abstract class following the template pattern, for grasp algorythms

---
+++ src/grasp_algorythm.cc
@@ -0,0 +1,43 @@
+// University of La Laguna
+// School of Engineering and Technology
+// Bachelor's Degree in Computer Engineering
+// Subject: Design and Analysis of Algorithms
+// Course: 3rd
+// Practice 6: Multi-Source Capacitated Facility Location Problem with Customer Incompatibilities
+// Author: Javier Acosta Portocarrero
+// Date: 04/04/2026
+// File grasp-algorythm.cc: implementation file.
+// Contains the implementation of the GraspAlgorythm, class.
+
+#include <stdexcept>
+
+#include "grasp_algorythm.h"
+
+/**
+ * @brief Solves the problem using the GRASP metaheuristic.
+ *
+ * The method implements the template pattern, where the specific steps
+ * of the algorithm are defined by the concrete implementations of the class.
+ *
+ * @param input Pointer to the problem instance to solve.
+ * @return A pointer to the best solution found for the given instance.
+ */
+Solution* GraspAlgorythm::Solve(Instance* input) {
+  if (input == nullptr) {
+    throw std::invalid_argument("Null instance.");
+  }
+  
+  Preprocess(input);
+  Solution* best_solution = nullptr;
+  do {
+    Solution* current_solution = ConstructSolution(input);
+    if (current_solution == nullptr) {
+      throw std::runtime_error("ConstructSolution returned null.");
+    }
+    Postprocess(current_solution);
+    UpdateBest(current_solution, best_solution);
+    delete current_solution;
+  } while (!StopCriterion());
+
+  return best_solution;
+}
---
+++ src/grasp_algorythm.h
@@ -0,0 +1,35 @@
+// University of La Laguna
+// School of Engineering and Technology
+// Bachelor's Degree in Computer Engineering
+// Subject: Design and Analysis of Algorithms
+// Course: 3rd
+// Practice 6: Multi-Source Capacitated Facility Location Problem with Customer Incompatibilities
+// Author: Javier Acosta Portocarrero
+// Date: 04/04/2026
+// File grasp-algorythm.h: declaration file.
+// Contains the declaration of the GraspAlgorythm, class.
+
+#ifndef GRAASP_ALGORYTHM_H_
+#define GRAASP_ALGORYTHM_H_
+
+#include "algorythm.h"
+
+/**
+  * @class GraspAlgorythm
+  * 
+  * @brief Abstract class that implements the GRASP metaheuristic, it follows the template method pattern.
+  *        The concrete implementations are responsible for defining the specific steps of the algorithm.
+  */
+class GraspAlgorythm : public Algorythm {
+ public:
+  Solution* Solve(Instance* input) override;
+
+ protected:
+  virtual void Preprocess(Instance* input) = 0;
+  virtual Solution* ConstructSolution(Instance* input) = 0;
+  virtual void Postprocess(Solution* solution) = 0;  
+  virtual void UpdateBest(Solution* current, Solution*& best) = 0;
+  virtual bool StopCriterion() const = 0;
+};
+
+#endif
